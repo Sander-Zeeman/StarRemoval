752b7842d0c042f195e2134b8ea068bd190efb39
Sander <szeeman2000@gmail.com>
2021-05-20 20:31:12 +0200

    Added index and id to Point, as this will be needed in the clustering.

--- include/Point.h
+++ include/Point.h
@@ -6,11 +6,17 @@
 class Point
 {
 public:
-    Point(float x, float y) : m_x(x), m_y(y) {};
+    Point(long index, float area, float value)
+        : m_index(index), m_x(area), m_y(value) {};
     ~Point() {};
 
     float x() { return m_x; }
     float y() { return m_y; }
+    long index() { return m_index; }
+    short id() { return m_id; }
+
+    void setID(short id) { m_id = id; }
+
     float euclidSquaredDist(Point p) {
         float xDist = abs(p.x() - m_x);
         float yDist = abs(p.y() - m_y);
@@ -20,6 +26,8 @@ public:
 private:
     float m_x { 0.0f };
     float m_y { 0.0f };
+    long index { -1 };
+    short m_id { -1 };
 };
 
 #endif
